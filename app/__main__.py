fbd30679b7b59e2dd6c13f967adbdb079a570439
imkmz <massa@Antoines-MacBook-Air.local>
2025-11-26 18:28:10 +0100

    a

---
+++ app/__main__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Test Python application for Whanos
+Execution: python -m app
+"""
+
+if __name__ == "__main__":
+    print("[INFO] Hello from Whanos Python app!")
+    print("[SUCCESS] Python module execution successful")
